7bb27030021fbd67d4997c61dc5ea73ae9a7f274
kurshid678 <153853301+kurshid678@users.noreply.github.com>
2025-07-10 20:30:34 +0530

    Updated vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { Buffer } from 'buffer';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    global: 'window',
+    Buffer: Buffer,
+    'process.env': {},
+    'process.env.GOOGLE_SDK_NODE_LOGGING': '{}',
+    'process.stdout': {},
+    'process.stderr': {},
+  },
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer',
+      events: 'events',
+    },
+  },
+});
